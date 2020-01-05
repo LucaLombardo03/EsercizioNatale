a90558faf3e95934e3aacd910a2467502078aa06
luca lombardo <lucalombardo03@gmail.com>
2020-01-05 17:41:39 +0100

    add file valido, add file invalido

--- EsercizioNatale/MainWindow.xaml.cs
+++ EsercizioNatale/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.IO;
 
 namespace EsercizioNatale
 {
-    /// <summary>
+    /// <summary> 
     /// Logica di interazione per MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
